d204db8627df8ad1deee45e98e0abd1b8cca886f
Mouzan Raza <mouzan.ai.dev@gmail.com>
2026-02-06 16:34:34 +0500

    Model updated

--- langGraph_backend.py
+++ langGraph_backend.py
@@ -186,7 +186,7 @@ def template_node(state: GraphState):
 
 
 load_dotenv()
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 def llm_node(state: GraphState):
     messages = [
         SystemMessage(
